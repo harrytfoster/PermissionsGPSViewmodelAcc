2be905b6ea759ab7f375b0d34547259792ad9d35
Harry <harrythomasfoster@gmail.com>
2024-03-22 19:49:00 +0000

    .

--- app/src/main/java/com/example/permissionsgpsviewmodelacc/MainActivity.kt
+++ app/src/main/java/com/example/permissionsgpsviewmodelacc/MainActivity.kt
@@ -22,10 +22,16 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.annotation.RequiresPermission
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import org.osmdroid.util.GeoPoint
 
 data class LatLon(val lat: Double, val lon: Double) // our LatLon class
 
@@ -40,39 +46,54 @@ class LatLonViewModel: ViewModel() {
 }
 
 
+
 class MainActivity : ComponentActivity(), LocationListener { // LocationListener handles location updates
-    val viewModel : LatLonViewModel by viewModels() // Access ViewModel
+    val viewModel: LatLonViewModel by viewModels() // Access ViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         checkPermissions()
         setContent {
-            DisplayCoords()
+            DisplayCoords(LatLonViewModel())
         }
     }
 
     fun checkPermissions() {
-        val requiredPermission = Manifest.permission.ACCESS_FINE_LOCATION // Choose permission to use
-        if(checkSelfPermission(requiredPermission) == PackageManager.PERMISSION_GRANTED) { // Checks is permission is already granted
+        val requiredPermission =
+            Manifest.permission.ACCESS_FINE_LOCATION // Choose permission to use
+        if (checkSelfPermission(requiredPermission) == PackageManager.PERMISSION_GRANTED) { // Checks is permission is already granted
             startGPS()
         } else { // If permission is not already granted
-            val permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted -> // Request Permission
-                if(isGranted) {
-                    startGPS()
-                } else {
-                    Toast.makeText(this, "GPS permission not granted", Toast.LENGTH_LONG).show() // Show this is permission is not granted
+            val permissionLauncher =
+                registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted -> // Request Permission
+                    if (isGranted) {
+                        startGPS()
+                    } else {
+                        Toast.makeText(this, "GPS permission not granted", Toast.LENGTH_LONG)
+                            .show() // Show this is permission is not granted
+                    }
                 }
-            }
             permissionLauncher.launch(requiredPermission) // Launches permission
         }
     }
+
     @SuppressLint("MissingPermission") // NEED THIS
     fun startGPS() {
-        val mgr = getSystemService(LOCATION_SERVICE) as LocationManager // System obtains default location manager
-        mgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, this ) // Update app with new location information
+        val mgr =
+            getSystemService(LOCATION_SERVICE) as LocationManager // System obtains default location manager
+        mgr.requestLocationUpdates(
+            LocationManager.GPS_PROVIDER,
+            0,
+            0f,
+            this
+        ) // Update app with new location information
     }
 
     override fun onLocationChanged(location: Location) {
-        Toast.makeText(this, "Latitude: ${location.latitude}, Longitude: ${location.longitude}", Toast.LENGTH_LONG).show()
+        Toast.makeText(
+            this,
+            "Latitude: ${location.latitude}, Longitude: ${location.longitude}",
+            Toast.LENGTH_LONG
+        ).show()
         viewModel.latLon = LatLon(location.latitude, location.longitude)
     }
 
@@ -88,13 +109,16 @@ class MainActivity : ComponentActivity(), LocationListener { // LocationListener
     override fun onStatusChanged(provider: String, status: Int, extras: Bundle) { // NEED THIS TOO
 
     }
+}
+
 
     @Composable
-    fun DisplayCoords(){
-        Text("Latitude: Longitude:")
-        viewModel.latLonLiveData.observe(this){
-            var Obslat = it.lat.toString()
-            var Obslon = it.lon.toString()
+    fun DisplayCoords(latLonViewModel: LatLonViewModel) {
+        var geoLocation by remember { mutableStateOf(GeoPoint(0.0, 0.0)) }
+        val context = LocalContext.current
+        latLonViewModel.latLonLiveData.observe(context as ComponentActivity){LatLon ->
+            geoLocation = GeoPoint(LatLon.lat, LatLon.lon)
         }
+        Text("Latitude: ${geoLocation.latitude} Longitude: ${geoLocation.longitude}")
     }
-}
+
